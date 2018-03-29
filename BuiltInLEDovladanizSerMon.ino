f3bd7317a12062f841872bfb250ec9f38951b178
MartinStrnad <nemanice@seznam.cz>
2018-03-29 14:55:20 +0200

    prepinani LED tlacitkem a ze SM

---
+++ BuiltInLEDovladanizSerMon.ino
@@ -0,0 +1,59 @@
+
+// this constant won't change:
+const int inPin = 2;   // choose the input pin (for a pushbutton)
+
+// Variables will change:
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+int pin13state = 1;          // kdyby = 0, tak ledka sviti hned po nahrani programu
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);    // declare LED as input
+  pinMode(inPin, INPUT_PULLUP);    // declare pushbutton as input....vyuzit internal pull up rezistor
+}
+
+
+void loop() {
+ if (Serial.available()) {
+    delay(50);                               // wait a bit for the entire message to arrive
+    while (Serial.available() > 0) {          // read all the available characters
+      String ch;
+        ch = Serial.readString();
+        ch.trim();
+        if(ch=="zapni"||ch=="ZAPNI"){
+          digitalWrite(LED_BUILTIN, HIGH);
+          Serial.println("Mas to zaply");       
+        }
+        else if(ch=="vypni"||ch=="VYPNI"){
+          digitalWrite(LED_BUILTIN, LOW);
+          Serial.println("Mas to vyply");          
+        }
+        else {
+          Serial.println("Neplatny vyraz");
+        }
+    }
+ }
+        buttonState = digitalRead(inPin);           // read the pushbutton input pin:
+        
+   if (buttonState != lastButtonState) {            // compare the buttonState to its previous state
+     if (buttonState == HIGH) {                   
+     if (pin13state == 0) pin13state = 1;
+     else                 pin13state = 0;
+     delay(100);                                      // Delay a little bit to avoid bouncing
+  }  
+    lastButtonState = buttonState;                  // save the current state as the last state, for next time through the loop   
+    digitalWrite(LED_BUILTIN, pin13state); 
+    }
+ }
+ 
+   
+  
+  /*
+ Invert button: https://www.arduino.cc/en/Tutorial/Invert
+ Input Pull-up Serial https://www.arduino.cc/en/Tutorial/InputPullupSerial
+ Button:  https://www.arduino.cc/en/Tutorial/Button
+ https://www.youtube.com/watch?v=jYOYgU2vlSE
+ https://www.youtube.com/watch?v=SB_fLg62Z1E
+ */      
